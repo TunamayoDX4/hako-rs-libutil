54abcf8aa90fa64f49ed5ae9a2c78b5f27efe975
TunamayoDX4 <tsunamayodx4@outlook.jp>
2023-11-26 19:37:27 +0900

    preludeの追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
 //! 箱庭諸島に使うユーティリティ的なのの実装
 
 pub mod pos;
+
+pub mod prelude {
+    pub use crate::pos::{
+        HMapSize, 
+        HMapPos, 
+        HMapDist, 
+        neighbor::neigh_hex, 
+        artable::AroundDistTbl, 
+    };
+}
